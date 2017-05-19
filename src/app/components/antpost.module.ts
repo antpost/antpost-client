f5680ddc26f269910e2619fd641e4e3e65a5d9bc
Cuongnm <cuong.nguyen@citigo.com.vn>
2017-05-19 18:50:15 +0700

    datatable

--- src/app/components/antpost.module.ts
+++ src/app/components/antpost.module.ts
@@ -3,11 +3,13 @@ import { RouterModule } from '@angular/router';
 import { MODULE_COMPONENTS, MODULE_ROUTES } from './antpost.routes';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 
 @NgModule({
     imports: [
         FormsModule,
         HttpModule,
+        NgxDatatableModule,
         RouterModule.forChild(MODULE_ROUTES)
     ],
     declarations: [ MODULE_COMPONENTS ]
