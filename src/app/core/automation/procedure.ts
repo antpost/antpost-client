e1e885659ebe9ee27552ee9b7e86115b67d91b6c
Cuongnm <cuong.nguyen@citigo.com.vn>
2017-06-29 18:54:58 +0700

    procedure

---
+++ src/app/core/automation/procedure.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Nguyen Manh Cuong on 6/29/2017.
+ */
+
+export class Procedure {
+    public actions;
+    public errorRule;
+    public successRule;
+}
--- src/app/services/automation.service.ts
+++ src/app/services/automation.service.ts
@@ -4,11 +4,27 @@
 
 import {ProxyService} from "./proxy.service";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {Procedure} from "../core/automation/procedure";
 
 @Injectable()
 export class AutomationService extends ProxyService {
 
     public login(username: string, password: string) {
 
+    }
+
+    /**
+     *
+     * @param procedure
+     * @returns {Observable<R>}
+     */
+    protected simulate(procedure: Procedure): Observable<any> {
+        return this.http.post(`${this.host}/simulate`, procedure)
+            .map((response: Response) => {
+                return response.json();
+            }).catch((error: Response) => {
+                return Observable.throw(error.json().error || 'Server error');
+            });
     }
 }
